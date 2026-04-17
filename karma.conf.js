d6f400975da75a5b9e573866a4edfc6e7a95d30a
Juan Pablo <giraldoatehortuapaj@gmail.com>
2026-04-17 11:21:30 -0500

    Karma config

--- karma.conf.js
+++ karma.conf.js
@@ -1,28 +1,12 @@
-const path = require('path');
-
 module.exports = function (config) {
   config.set({
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
-
-    browsers: ['ChromeHeadlessNoSandbox'],
+    browsers: ['ChromeHeadless'],
 
     customLaunchers: {
-      ChromeHeadlessNoSandbox: {
+      ChromeHeadless: {
         base: 'ChromeHeadless',
         flags: ['--no-sandbox', '--disable-gpu']
       }
-    },
-
-    reporters: ['progress', 'kjhtml', 'coverage'],
-
-    coverageReporter: {
-      dir: path.join(__dirname, './coverage'),
-      subdir: '.',
-      reporters: [
-        { type: 'html' },
-        { type: 'lcov' },
-        { type: 'text-summary' }
-      ]
     }
   });
 };
