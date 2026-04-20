845fd787d33cc7f37c17c14735b3abf7ca92baad
Juan Pablo <giraldoatehortuapaj@gmail.com>
2026-04-20 10:40:31 -0500

    Karma

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,8 @@
 module.exports = function (config) {
   config.set({
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
     reporters: ['progress', 'coverage'],
+
     coverageReporter: {
       dir: require('path').join(__dirname, './coverage/pruebas-demo-front'),
       subdir: '.',
