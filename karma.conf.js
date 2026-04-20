3e5d041c2e8514208006dcccc7516706f68b6f50
Juan Pablo <giraldoatehortuapaj@gmail.com>
2026-04-20 10:45:45 -0500

    Dependencias y karma

--- karma.conf.js
+++ karma.conf.js
@@ -1,15 +1,28 @@
+const path = require('path');
+
 module.exports = function (config) {
   config.set({
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
+
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+
     reporters: ['progress', 'coverage'],
 
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/pruebas-demo-front'),
+      dir: path.join(__dirname, './coverage/pruebas-demo-front'),
       subdir: '.',
       reporters: [
         { type: 'html' },
         { type: 'lcovonly' }
       ]
-    }
+    },
+
+    browsers: ['ChromeHeadless'],
+    singleRun: true
   });
 };
